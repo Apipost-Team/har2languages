f08c7a971571c113b4e0c467109db5b58054f204
宣博文 <1030698842@qq.com>
2022-07-08 00:27:13 +0800

    update package.json

--- src/har2languages.ts
+++ src/har2languages.ts
@@ -1,32 +1,38 @@
-const HTTPSnippet=require('httpsnippet');
-
-class har2languages{
-    languages : any;
-    constructor(){
-        this.languages={
-            c:['libcurl'],
-            clojure:['clj_http'],
-            csharp:['httpclient','restsharp'],
-            go:['native'],
-            http:['1.1'],
-            java:['asynchttp','nethttp','okhttp','unirest'],
-            javascript:['axios','fetch','jquery','xhr'],
-            kotlin:['okhttp'],
-            node:['axios','fetch','native','request','unirest'],
-            objc:['nsurlsession'],
-            ocaml:['cohttp'],
-            php:['curl','http1','http2'],
-            powershell:['webrequest','restmethod'],
-            python:['python3','requests'],
-            r:['httr'],
-            ruby:['native'],
-            shell:['curl','httpie','wget'],
-            swift:['nsurlsession']
+import Atools from 'apipost-tools';
+const HTTPSnippet = require('httpsnippet');
+class har2languages {
+    languages: any;
+    constructor() {
+        this.languages = {
+            shell: ['curl', 'httpie', 'wget'],
+            c: ['libcurl'],
+            clojure: ['clj_http'],
+            csharp: ['httpclient', 'restsharp'],
+            go: ['native'],
+            http: ['1.1'],
+            java: ['asynchttp', 'nethttp', 'okhttp', 'unirest'],
+            javascript: ['axios', 'fetch', 'jquery', 'xhr'],
+            kotlin: ['okhttp'],
+            node: ['axios', 'fetch', 'native', 'request', 'unirest'],
+            objc: ['nsurlsession'],
+            ocaml: ['cohttp'],
+            php: ['curl', 'http1', 'http2'],
+            powershell: ['webrequest', 'restmethod'],
+            python: ['python3', 'requests'],
+            r: ['httr'],
+            ruby: ['native'],
+            swift: ['nsurlsession']
         }
     }
-    convert(har : Object,language : String,type : String = ''){
-        const snippet = new HTTPSnippet(har);
-        return snippet.convert(language,type);
+    convert(har: Object, language: String, type: String = '') {
+        try {
+            const snippet = new HTTPSnippet(har);
+            const result = snippet.convert(language, type);
+            return Atools.successResult(result)
+        } catch (error) {
+            return Atools.errorResult(String(error))
+        }
+
     }
 }
 
--- test/har2languages.test.ts
+++ test/har2languages.test.ts
@@ -3,25 +3,43 @@ let path = require('path')
 let fs = require('fs')
 
 describe('works', () => {
-  let har={
+  let har = {
     "method": "GET",
     "url": "http://www.example.com/path/?param=value",
     "httpVersion": "HTTP/1.1",
     "cookies": [],
-    "headers": [],
-    "queryString" : [],
-    "postData" : {},
-    "headersSize" : 150,
-    "bodySize" : 0,
-    "comment" : ""
-}
+    "headers": [{
+      "name": "Accept-Encoding",
+      "value": "gzip,deflate",
+      "comment": ""
+    },
+    {
+      "name": "Accept-Language",
+      "value": "en-us,en;q=0.5",
+      "comment": ""
+    }],
+    "queryString": [{
+      "name": "param1",
+      "value": "value1",
+      "comment": ""
+    },
+    {
+      "name": "param1",
+      "value": "value1",
+      "comment": ""
+    }],
+    "postData": {},
+    "headersSize": 150,
+    "bodySize": 0,
+    "comment": ""
+  }
   let Har2languages = new har2languages();
   // Object.keys(Har2languages.languages).forEach(key=>{
   //   for (const type of Har2languages.languages[key]) {
   //       console.log(`${key}:${type}`,Har2languages.convert(har,key,type));
   //   }
   // })
-  let nodeRequest=`const request = require('request');
+  let nodeRequest = `const request = require('request');
     
   const options = {method: 'GET', url: 'http://www.example.com/path/', qs: {param: 'value'}};
   
@@ -31,7 +49,7 @@ describe('works', () => {
     console.log(body);
   });`
   it('curl2har test', () => {
-    expect(!!Har2languages.convert(har,'node','request')).toEqual(true);
+    expect(!!Har2languages.convert(har, 'php', 'curl')).toEqual(true);
   });
 });
 
